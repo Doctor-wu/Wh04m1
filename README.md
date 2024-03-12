fc618ffece8edd684a937e332e399f82d3352177
Doctor Wu <44631608+Doctor-wu@users.noreply.github.com>
2024-03-13 00:06:01 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+<h1 align='center'>⚡️ VueUse Vite Starter</h1>
+
+<p align='center'>
+<img src='./src/logo.svg' height='120'/>
+</p>
+
+
+Starter for [Vite](https://github.com/vuejs/vite) + [VueUse](https://github.com/vueuse/vueuse) + [TypeScript](https://www.typescriptlang.org/)
+
+🌐 [Live Demo](https://vueuse-vite-starter.netlify.app/)
+
+📦 [Webpack Example](https://github.com/vueuse/vueuse-vue3-example)
+
+## Installation
+
+1. Clone this repository
+2. `npm i`
+3. `npm run dev`
+4. open http://localhost:3000 in your browser
+
+## More Example
+
+Enjoy using Vite and VueUse? Check out the real world template [Vitesse](https://github.com/antfu/vitesse)!
+
+## License
+
+[MIT](http://opensource.org/licenses/MIT)
---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="app">
+    <img src="./logo.svg" />
+    <h1>Hello <a href='https://github.com/vuejs/vite' target='__blank'>Vite</a> and <a href='https://github.com/vueuse/vueuse' target='__blank'>VueUse</a>!</h1>
+
+    <h3>Mouse: {{x}} x {{y}}</h3>
+    <h3>
+      Counter: {{count}}
+      <a @click='inc()' style='margin-right:10px'>+</a>
+      <a @click='dec()'>-</a>
+    </h3>
+
+    <br><br>
+    <p><a href='https://github.com/vueuse/vueuse-vite-starter' target='__blank'>Source</a></p>
+    <p><a href='https://vueuse-vue3-example.netlify.app/' target='__blank'>Webpack Example</a></p>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useMouse, useCounter } from '@vueuse/core'
+
+const { x, y } = useMouse()
+const { count, inc, dec } = useCounter()
+</script>
+
+<style scoped>
+html, body, h1, h2, h3, p {
+  font-family: 'Noto Serif', serif;
+  user-select: none;
+}
+
+#app {
+  text-align: center;
+  color: rgba(0,0,0,0.4);
+}
+img {
+  width: 500px;
+}
+a {
+  color: #41b883;
+  text-decoration: none;
+  cursor: pointer;
+}
+</style>

f310a385e8af384476f02512aa821c248f544857
Doctorwu <doctorwu@moego.pet>
2024-03-27 11:43:12 +0800

    feat(intro): tweak style

--- src/components/widgets/PeopleWithAvatar.vue
+++ src/components/widgets/PeopleWithAvatar.vue
@@ -8,11 +8,12 @@ defineProps<{
 
 <template>
   <a
-    class="!transition-all !border-none !no-underline cursor-pointer !text-inherit rounded-4 !h-6 pr-2 py-[0px] vertical-middle inline-flex items-center justify-start hover:bg-gray-100 hover:dark:bg-gray-800 "
+    class="!transition-all !border-none !no-underline cursor-pointer !text-inherit inline-flex rounded-4 !h-6 pr-2 py-[0px] vertical-middle hover:bg-gray-100 hover:dark:bg-gray-800"
+    flex="items-center justify-start"
     :href="link ?? 'javascript: void 0;'"
     target="_blank"
   >
-    <img class="inline !rounded-4 h-full !w-6 !m0" :src="avatarUrl">
+    <img class="inline !rounded-4 h-5 !w-5 ml-[2px]" :src="avatarUrl">
     <span class="ml-1 text-sm">{{ name }}</span>
   </a>
 </template>

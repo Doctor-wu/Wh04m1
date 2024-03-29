7c23ce964534d7fd1c83679a0e5a640194fa2b18
Doctorwu <doctorwu@moego.pet>
2024-03-29 16:12:43 +0800

    feat(component): update person-with-avatar ui

--- src/components/widgets/PeopleWithAvatar.vue
+++ src/components/widgets/PeopleWithAvatar.vue
@@ -8,12 +8,39 @@ defineProps<{
 
 <template>
   <a
-    class="!transition-all !border-none !no-underline cursor-pointer !text-inherit inline-flex rounded-4 !h-6 pr-2 py-[0px] vertical-mid hover:bg-gray-200 hover:dark:bg-gray-500 !bg-op-30"
+    class="base !border-none !no-underline cursor-pointer !text-inherit inline-flex rounded-4 !h-6 pr-2 py-[0px] vertical-mid box-content relative"
+    hover="bg-gray-100 dark:bg-gray-500 !bg-op40 !outline-1 !outline-gray-3:50 !dark:outline-gray-4:10 !outline-solid"
     flex="items-center justify-start" :href="link ?? 'javascript: void 0;'" target="_blank"
   >
-    <img class="inline !rounded-4 h-5 !w-5 ml-[2px]" :src="avatarUrl">
+    <img class="inline !rounded-4 h-5 !w-5 ml-[2px]" :src="avatarUrl" alt="Avatar">
     <span class="ml-1 text-sm">{{ name }}</span>
   </a>
 </template>
 
-<style lang="scss" scoped></style>
+<style scoped>
+.base {
+  overflow: hidden;
+}
+
+.base::before {
+  content: '';
+  position: absolute;
+  left: -110%;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  background-image: linear-gradient(
+    90deg,
+    rgba(255, 255, 255, 0),
+    rgba(255, 255, 255, 0.5),
+    rgba(255, 255, 255, 0)
+  );
+}
+
+.base:hover {
+  &::before {
+    left: 110%;
+    transition: all 0.8s;
+  }
+}
+</style>
